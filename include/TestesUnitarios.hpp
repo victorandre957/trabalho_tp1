15c98c4fe3bab3a7ed0cab968521b28183a371d4
Victor André <victorandre957@gmail.com>
2023-04-11 14:14:00 -0300

    Criar classes para os testes unitarios

---
+++ include/TestesUnitarios.hpp
@@ -0,0 +1,100 @@
+#ifndef TESTSUNITARIOS_HPP
+#define TESTSUNITARIOS_HPP
+
+#include "Dominios.hpp"
+#include "Entidades.hpp"
+
+class TestDominio {
+    protected:
+        bool estado;
+        Dominio * dominio;
+        const string * VALOR_VALIDO;
+        const string * VALOR_INVALIDO;
+        virtual void setUp() = 0;
+        void tearDown();
+        virtual void testarCasoValido();
+        virtual void testarCasoInvalido();
+
+    public:
+        bool run();
+};
+
+class TestCodigo:public TestDominio{
+    private:
+        void setUp();
+};
+
+class TestData:public TestDominio{
+    private:
+        void setUp();
+};
+
+class TestMatricola:public TestDominio{
+    private:
+        void setUp();
+};
+
+class TestResultado:public TestDominio{
+    private:
+        void setUp();
+};
+
+class TestSenha:public TestDominio{
+    private:
+        void setUp();
+};
+
+class TestTelefone:public TestDominio{
+    private:
+        void setUp();
+};
+
+class TestTexto:public TestDominio{
+    private:
+        void setUp();
+};
+
+class TestClasse:public TestDominio{
+    private:
+        void setUp();
+};
+
+class TestTeste {
+private:
+    Teste *teste;
+    bool estado;
+    void setUp();
+    void tearDown();
+    void testarCenarioSucesso();
+public:
+    bool run();
+};
+
+class TestCasoDeTeste {
+    private:
+        CasoDeTeste *casoDeTeste;
+        bool estado;
+        void setUp();
+        void tearDown();
+        void testarCenarioSucesso();
+    public:
+        bool run();
+};
+
+class TestDesenvolvedor {
+    private:
+       Desenvolvedor * desenvolvedor;
+       const string * MATRICULA_VALIDA;
+       const string * NOME_VALIDO;
+       const string * SENHA_VALIDA;
+       const string * TELEFONE_VALIDO;
+
+       bool estado;
+       void setUp();
+       void testarCenarioSucesso();
+       void tearDown();
+    public:
+        bool run();
+};
+
+#endif // TESTSUNITARIOS_HPP
